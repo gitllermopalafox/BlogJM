2690d1f09fe87d3c77662fb51bbb17fae6e6aee8
programacion3 <programacion3@axovia.mx>
2017-02-28 14:57:52 -0600

    Update routes & lang

--- src/PackageServiceProvider.php
+++ src/PackageServiceProvider.php
@@ -22,7 +22,9 @@ class PackageServiceProvider extends ServiceProvider {
 		$this->publishes([ __DIR__.'/public/styl' => base_path('resources/assets/styl')]);
 		$this->publishes([ __DIR__.'/public/images/blog' => base_path('public/images/blog')]);
 		$this->publishes([ __DIR__.'/public/uploads/blog' => base_path('public/uploads/blog')]);
-		$this->publishes([ __DIR__.'/config' => base_path('config')]);
+		$this->publishes([ __DIR__.'/config/translatable.php' => base_path('config')]);
+		$this->publishes([ __DIR__.'/lang/en/blog.php' => base_path('resources/lang/en/')]);
+		$this->publishes([ __DIR__.'/lang/es/blog.php' => base_path('resources/lang/es/')]);
 	}
 
 	/**
